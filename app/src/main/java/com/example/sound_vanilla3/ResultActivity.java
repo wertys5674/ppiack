1301ab291e43851968131f78ddbd960b018f9460
wertys5674 <wertys5674@naver.com>
2022-06-05 17:29:33 +0900

    2022/6/5 수정

---
+++ app/src/main/java/com/example/sound_vanilla3/ResultActivity.java
@@ -0,0 +1,75 @@
+package com.example.sound_vanilla3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ResultActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Bitmap bitmap;
+    Button btn_GoMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        btn_GoMain = findViewById(R.id.btn_GoMain);
+
+        btn_GoMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ResultActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //안드로이드에서 네트워크와 관련된 작업을 할 때,
+        //반드시 메인 Thread가 아닌 별도의 작업 Thread를 생성하여 작업해야 한다.
+        Thread uThread = new Thread() {
+            @Override
+            public void run(){
+                try{
+                    //서버에 올려둔 이미지 URL
+                    URL url = new URL("http://___이미지경로____/me.jpg");
+
+                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+
+                    conn.setDoInput(true); //Server 통신에서 입력 가능한 상태로 만듦
+                    conn.connect(); //연결된 곳에 접속할 때 (connect() 호출해야 실제 통신 가능함)
+
+
+                    InputStream is = conn.getInputStream(); //inputStream 값 가져오기
+                    bitmap = BitmapFactory.decodeStream(is); // Bitmap으로 반환
+
+                }catch (MalformedURLException e){
+                    e.printStackTrace();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        uThread.start(); // 작업 Thread 실행
+        try{
+            uThread.join();
+            imageView.setImageBitmap(bitmap);
+
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
